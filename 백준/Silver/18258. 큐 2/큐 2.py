2cbe4de71ef1c40da776f3eb048d803e76290ff1
kimfield <141253939+kimfield98@users.noreply.github.com>
2023-08-19 14:33:09 +0900

    [Silver IV] Title: 큐 2, Time: 1552 ms, Memory: 175204 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque # 모듈 불러오기
+input = sys.stdin.readline
+
+queue = deque([]) # deque로 감싸준다.
+N = int(input())
+for i in range(N):
+    command = input().split()
+
+    if command[0] == 'push':
+        queue.append(command[1])
+    elif command[0] == 'pop':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue.popleft()) # 맨 앞 인덱스 빼기
+    elif command[0] == 'size':
+        print(len(queue))
+    elif command[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'front':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+    elif command[0] == 'back':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[-1])
