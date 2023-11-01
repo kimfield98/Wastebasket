73e13dde2b78640a4039dec160cee89ae06999d3
kimfield98 <kimfield98@gmail.com>
2023-11-01 15:14:53 +0900

    컴포넌트 만들기 실습 (2)

--- firstpjt/src/App.js
+++ firstpjt/src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-function App() {
-  
-	// <---- 자바스크립트 영역 ---->
-  const handleClick = () => {
-    alert('클릭!');
-  }
 
-  return (
-  /* <---- HTML/JSX 영역  ---->*/
-    <div
-      style={{
-        height: '100vh',
-        display: ' flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <span>이것은 내가 만든 App컴포넌트 입니다</span>
-      <button onClick={handleClick}>클릭!</button>
-    </div>
-  );
+function Child() {
+  return <div>연결 성공</div>;
+}
+
+function Mother() {
+  return <Child/>;
+}
+
+function GrandFather() {
+  return <Mother/>;
+}
+
+function App() {
+  return <GrandFather/>;
 }
 
 export default App;
