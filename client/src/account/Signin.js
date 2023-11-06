f66806ac5bc702f68b8f75dee982beb92817f408
kimfield98 <kimfield98@gmail.com>
2023-11-07 07:13:47 +0900

    React JSX 코드 검사 수정

--- client/src/account/Signin.js
+++ client/src/account/Signin.js
@@ -1,33 +1,43 @@
 import React, {useState} from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import Header from '../board/Header';
 import '../css/App.css';
 import '../css/Reset.css';
  
 function Signin(){
-  const users = [
-    {username: "김초원",password:"1234"},
-    {username: "장호영",password:"5678"},
-  ]
+  // const users = [
+  //   {username: "김초원",password:"1234"},
+  //   {username: "장호영",password:"5678"},
+  // ]
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-      e.preventDefault(); // 폼 제출 기본 동작 방지
-
-      // 입력된 username과 password를 검사
-      const user = users.find(u => u.username === username && u.password === password);
-
-      if (user) {
-          alert('로그인 성공!');
-          navigate('/');
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // 폼 제출 기본 동작 방지
+  
+    // 입력된 username와 password를 서버에 전송
+    try {
+      const response = await axios.post('/REACT_APP_API_URL', {
+        email: username, // 이메일 대신 username 사용
+        password: password
+      });
+  
+      if (response.data.loginSuccess) {
+        // 로그인 성공 후에 원하는 동작 수행
+        navigate('/');
       } else {
-          alert('로그인 실패. 올바른 사용자 이름과 비밀번호를 입력하세요.');
+        alert(response.data.message); // 서버로부터의 에러 메시지 표시
       }
-    };
+    } catch (error) {
+      alert('서버 오류. 다시 시도하십시오.');
+      console.error(error);
+    }
+  };
+  
   
   return (
       <>
