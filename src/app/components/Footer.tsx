d490e488bf27f76654bddf65787abf9d78516eda
kimfield98 <kimfield98@gmail.com>
2023-12-19 03:17:26 +0900

    :sparkles: Footer
    (+반응형)

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,19 +1,19 @@
 export default function Footer () {
   return (
     <div className="footer">
-      <div>
+      <div className="footer_item">
         <p className="text-lg font-bold">Phone</p>
         <p>+82-10-2449-2091</p>
       </div>
-      <div>
+      <div className="footer_item">
         <p className="text-lg font-bold">Email</p>
         <a href="mailto:kimfield98@gmail.com" className="footer_icon">kimfield98@gmail.com</a>
       </div>
-      <div>
+      <div className="footer_item">
         <p className="text-lg font-bold">Link</p>
         <a href="https://github.com/kimfield98/Portfolio_kimfield" target="_blank" rel="noopener noreferrer" className="footer_icon">github</a>
       </div>
-      <div>
+      <div className="footer_item">
         <p className="text-sm">© 2023 Kimfield. All Rights Reserved.</p>
       </div>
     </div>
