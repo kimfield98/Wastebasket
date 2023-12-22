bdfa559de4cd8bf194c63b20d1c3311a6167ef03
kimfield98 <kimfield98@gmail.com>
2023-12-22 22:33:24 +0900

    :sparkles: MainPage
    (+반응형)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import Footer from './components/Footer';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'CHOWON KIM',
+  title: 'CHOWON KIM PORTFOLIO',
   description: 'Kimfield WEB Portfolio',
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,26 @@
+"use client"
+
+import Link from "next/link";
+import { useRecoilState } from 'recoil';
+import { activeTabState } from './recoil/Atom'
+
 export default function Home() {
+  const [activeTab, setActiveTab] = useRecoilState(activeTabState);
   return (
     <div className="main">
       <div className="main_left">
-        <p className="main_icon0 circle w-96 h-96"></p>
+        <p className="main_image circle w-96 h-96"></p>
       </div>
       <div className="main_right">
-        <div className="main_right_top">
+        <div>
           <p className="text-6xl font-bold">안녕하세요</p>
           <p className="text-5xl font-bold">신입 프론트엔드 개발자</p>
-          <p className="p-1">열린 마음으로 상대의 의견을 듣는 김초원입니다.</p>
+          <p className="text-xl p-2">열린 마음으로 상대의 의견을 듣는 김초원입니다.</p>
         </div>
-        <div className="main_right_bottom">
-          <p className="main_icon1 circle text-xl font-bold w-32 h-32">Resume</p>
-          <p className="main_icon2 circle text-xl font-bold w-32 h-32">Project</p>
-          <p className="main_icon3 circle text-xl font-bold w-32 h-32">Contact</p>
+        <div className="flex gap-3 mt-3">
+          <Link href="/resume" className="orange circle w-32 h-32 text-xl font-bold">Resume</Link>
+          <Link href="/projects" className="red circle w-32 h-32 text-xl font-bold">Project</Link>
+          <Link href="/contact" className="blue circle w-32 h-32 text-xl font-bold">Contact</Link>
         </div>
       </div>
     </div>
