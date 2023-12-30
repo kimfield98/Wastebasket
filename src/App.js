dec6d190a669a63a10a9943199f851a1136c607b
kimfield98 <kimfield98@gmail.com>
2023-12-30 14:35:16 +0900

    :sparkles: Input, Button 만들기
    선언적 렌더링과 이벤트 핸들링

--- src/App.js
+++ src/App.js
@@ -8,7 +8,27 @@
 import { Component } from "./core/kimfield"
 
 export default class App extends Component {
+  constructor () {
+    super({
+      state: {
+        inputText: ''
+      }
+    })
+  }
   render() {
-    this.el.textContent = 'Hello, World!'
+    this.el.classList.add('search')
+    this.el.innerHTML = `
+    <input />
+    <button>Click!</button>
+    `
+    const inputEl = this.el.querySelector('input')
+    inputEl.addEventListener('input', () => {
+      this.state.inputText = inputEl.value
+    })
+
+    const buttonEl = this.el.querySelector('button')
+    buttonEl.addEventListener('click', () => {
+      console.log(this.state.inputText)
+    })
   }
 }
