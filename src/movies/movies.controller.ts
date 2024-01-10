6ac8c8d3234884a92aa1d2cd3ea1b4eb95973bfb
kimfield98 <kimfield98@gmail.com>
2024-01-11 00:59:34 +0900

    Validation

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Post, Delete, Param, Body, Patch, Query } from '@nestjs/common'
 import { MoviesService } from './movies.service'
 import { Movie } from './entities/movie.entity'
+import { CreateMovieDto } from './dto/create-movie.dto'
+import { UpdateMovieDto } from './dto/update-movie.dto'
 
 @Controller('movies')
 export class MoviesController {
@@ -13,22 +15,23 @@ export class MoviesController {
   }
 
   @Get(':id')
-  getOne(@Param('id') movieID:string):Movie {
+  getOne(@Param('id') movieID:number):Movie {
+    console.log(typeof movieID)
     return this.movieService.getOne(movieID)
   }
 
   @Post()
-  create(@Body() movieData) {
+  create(@Body() movieData: CreateMovieDto) {
     return this.movieService.create(movieData)
   }
 
   @Delete(':id')
-  remove(@Param('id') movieId:string) {
+  remove(@Param('id') movieId:number) {
     return this.movieService.deleteOne(movieId)
   }
 
   @Patch(':id')
-  path(@Param('id') movieId:string, @Body() updateData) {
+  path(@Param('id') movieId:number, @Body() updateData: UpdateMovieDto) {
     return this.movieService.update(movieId,updateData)
   }
 }
