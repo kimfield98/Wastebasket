4ba68ff37db339d7ed3738aad78cfd2877deec2a
kimfield98 <kimfield98@gmail.com>
2024-01-15 03:56:09 +0900

    :memo: Firebase 환경 세팅
    설치 및 관련 파일 생성

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA-dYvAypYKup-9KM86sEOsxLrqbcj23t0",
+  authDomain: "momyakson.firebaseapp.com",
+  projectId: "momyakson",
+  storageBucket: "momyakson.appspot.com",
+  messagingSenderId: "729758685544",
+  appId: "1:729758685544:web:03accfb363ebf74d081edd"
+};
+
+const app = initializeApp(firebaseConfig);
