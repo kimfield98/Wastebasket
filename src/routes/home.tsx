5438b092a4723eaab2e994bf86194288ead7f913
kimfield98 <kimfield98@gmail.com>
2024-01-16 02:19:47 +0900

    :sparkles: Post Form
    내용 작성, 사진 업로드 기능 추가

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,5 +1,14 @@
+import { styled } from "styled-components";
+import PostTweetForm from "../components/post-tweet-form";
+
+const Wrapper = styled.div`
+  
+`
+
 export default function Home() {
   return (
-    <h1></h1>
+    <Wrapper>
+      <PostTweetForm />
+    </Wrapper>
   )
 }
