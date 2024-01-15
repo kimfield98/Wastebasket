b44d7ba143ce3b575dd5383fd11313731d79362d
kimfield98 <kimfield98@gmail.com>
2024-01-16 00:48:04 +0900

    :sparkles: Navigation Bar
    홈, 프로필, 로그아웃 버튼 추가

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,27 +1,5 @@
-import { useState } from "react"
-import { auth } from "../firebase"
-import { useNavigate } from "react-router-dom"
-import { FirebaseError } from "firebase/app"
-
 export default function Home() {
-  const navigate = useNavigate()
-  const [error, setError] = useState("")
-  
-  const logOut = () => {
-    try {
-      auth.signOut()
-      navigate("/login")
-    } catch (e) {
-      if(e instanceof FirebaseError) {
-        setError(e.message)
-        console.log(error)
-      }
-    }
-  }
-
   return (
-    <h1>
-      <button onClick={logOut}>Log Out</button>
-      </h1>
+    <h1></h1>
   )
 }
