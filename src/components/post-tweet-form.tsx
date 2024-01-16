dfff6eb51c9c34c5847227b574c975f9ffd82593
kimfield98 <kimfield98@gmail.com>
2024-01-16 19:22:16 +0900

    :sparkles: Delete tweet
    트윗과 사진 함께 삭제하기

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,14 +1,15 @@
-import { addDoc, collection, updateDoc } from "firebase/firestore";
-import { useState } from "react";
-import { styled } from "styled-components";
-import { auth, db, storage } from "../firebase";
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { addDoc, collection, updateDoc } from "firebase/firestore"
+import { useState } from "react"
+import { styled } from "styled-components"
+import { auth, db, storage } from "../firebase"
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage"
 
 const Form = styled.form`
   display: flex;
   flex-direction: column;
   gap: 10px;
-`;
+`
+
 const TextArea = styled.textarea`
   border: 2px solid white;
   padding: 20px;
@@ -27,7 +28,8 @@ const TextArea = styled.textarea`
     outline: none;
     border-color: #1d9bf0;
   }
-`;
+`
+
 const AttachFileButton = styled.label`
   padding: 10px 0px;
   color: #1d9bf0;
@@ -37,10 +39,12 @@ const AttachFileButton = styled.label`
   font-size: 14px;
   font-weight: 600;
   cursor: pointer;
-`;
+`
+
 const AttachFileInput = styled.input`
   display: none;
-`;
+`
+
 const SubmitBtn = styled.input`
   background-color: #1d9bf0;
   color: white;
@@ -53,51 +57,49 @@ const SubmitBtn = styled.input`
   &:active {
     opacity: 0.9;
   }
-`;
+`
+
 export default function PostTweetForm() {
-  const [isLoading, setLoading] = useState(false);
-  const [tweet, setTweet] = useState("");
-  const [file, setFile] = useState<File | null>(null);
+  const [isLoading, setLoading] = useState(false)
+  const [tweet, setTweet] = useState("")
+  const [file, setFile] = useState<File | null>(null)
   const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setTweet(e.target.value);
-  };
+    setTweet(e.target.value)
+  }
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { files } = e.target;
+    const { files } = e.target
     if (files && files.length === 1) {
-      setFile(files[0]);
+      setFile(files[0])
     }
-  };
+  }
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const user = auth.currentUser;
-    if (!user || isLoading || tweet === "" || tweet.length > 180) return;
+    e.preventDefault()
+    const user = auth.currentUser
+    if (!user || isLoading || tweet === "" || tweet.length > 180) return
     try {
-      setLoading(true);
+      setLoading(true)
       const doc = await addDoc(collection(db, "tweets"), {
         tweet,
         createdAt: Date.now(),
         username: user.displayName || "Anonymous",
         userId: user.uid,
-      });
+      })
       if (file) {
-        const locationRef = ref(
-          storage,
-          `tweets/${user.uid}-${user.displayName}/${doc.id}`
-        );
-        const result = await uploadBytes(locationRef, file);
-        const url = await getDownloadURL(result.ref);
+        const locationRef = ref(storage, `tweets/${user.uid}/${doc.id}`)
+        const result = await uploadBytes(locationRef, file)
+        const url = await getDownloadURL(result.ref)
         await updateDoc(doc, {
           photo: url,
-        });
+        })
       }
-      setTweet("");
-      setFile(null);
+      setTweet("")
+      setFile(null)
     } catch (e) {
-      console.log(e);
+      console.log(e)
     } finally {
-      setLoading(false);
+      setLoading(false)
     }
-  };
+  }
   return (
     <Form onSubmit={onSubmit}>
       <TextArea
@@ -122,5 +124,5 @@ export default function PostTweetForm() {
         value={isLoading ? "Posting..." : "Post Tweet"}
       />
     </Form>
-  );
+  )
 }
