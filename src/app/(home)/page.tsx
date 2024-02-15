936a9f6346d8eeca114cd5c00346a7e445a21066
kimfield98 <kimfield98@gmail.com>
2024-02-15 17:37:32 +0900

    [STYLE] 메인페이지+네비게이션 바 UI 구현 #6

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,9 +2,14 @@ import NavigationBar from "../../components/NavigationBar";
 
 export default function Home() {
   return (
-    <div>
+    <div className='flex flex-col items-center gap-[15.17px] h-screen relative bg-[#26294C]'>
       <NavigationBar />
-      Home
+      <button className="w-[231.25px] h-[54px] absolute bottom-[107.38px] rounded-[30px] bg-gradient-to-r from-[#5F68FC] to-[#D071FF] text-white font-bold">
+        지금 신청하기
+      </button>
+      <button className="w-[231.25px] h-[54px] absolute bottom-[35.42px] rounded-[30px] border-2 border-[#B3B7BA] text-[#F2F4F7]">
+        혜택...이게 끝인가요?
+      </button>
     </div>
   );
 }
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,5 +1,12 @@
+import Link from 'next/link'
+import { Button } from "./ui/button";
+
 export default function NavigationBar() {
   return (
-    <div>NavigationBar</div>
+    <div className="flex justify-end items-center w-[100%] h-14 py-3 px-4 bg-white">
+      <Link href="/login">
+        <Button className="h-8 w-[67px] text-sm bg-[#393F7B]">로그인</Button>
+      </Link>
+    </div>
   );
 }
