1176d257ee0415607fb021d9628fa96a2759d87d
kimfield98 <kimfield98@gmail.com>
2024-02-15 12:29:27 +0900

    [FEAT]: zod, react-hook-form으로 로그인, 회원가입 폼 구현 #2

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,56 @@
+"use client"
+import { z, ZodType } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
+
+type FormData = {
+  email: string;
+  password: string;
+}
+
 export default function LoginComponent() {
+
+  const LoginSchema: ZodType<FormData> = z
+    .object({
+      email: z
+        .string()
+        .email("유효한 이메일 형식이 아닙니다."),
+      password: z
+        .string()
+        .min(9)
+        .max(20),
+    })
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormData>({resolver: zodResolver(LoginSchema)})
+
+  const submitData = (data: FormData) => {
+    console.log("일한다!", data)
+  }
+
   return (
-    <div>Login</div>
+    <section>
+      <header>
+        <h2>로그인</h2>
+      </header>
+      <form onSubmit={handleSubmit(submitData)}>
+        <div>
+          <label>이메일</label>
+          <input type="email" {...register("email")} />
+          {errors.email && <span>{errors.email.message}</span>}
+        </div>
+        <div>
+          <label>비밀번호</label>
+          <input type="password" {...register("password")} />
+          {errors.password && <span>{errors.password.message}</span>}
+        </div>
+        <div>
+          <button type="submit">로그인</button>
+        </div>
+      </form>
+    </section>
   );
 }
--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,5 +1,80 @@
+"use client"
+import { z, ZodType } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
+
+type FormData = {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
 export default function SignupComponent() {
+
+  const SignupSchema: ZodType<FormData> = z
+    .object({
+      name: z
+        .string()
+        .min(3)
+        .max(20),
+      email: z
+        .string()
+        .email("유효한 이메일 형식이 아닙니다."),
+      password: z
+        .string()
+        .min(9)
+        .max(20),
+      confirmPassword: z
+        .string()
+        .min(9)
+        .max(20),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: "비밀번호가 일치하지 않습니다",
+      path: ["confirmPassword"],
+    })
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormData>({resolver: zodResolver(SignupSchema)})
+
+  const submitData = (data: FormData) => {
+    console.log("일한다!", data)
+  }
+
   return (
-    <div>Signup</div>
+    <section>
+      <header>
+        <h2>회원가입</h2>
+      </header>
+      <form onSubmit={handleSubmit(submitData)}>
+        <div>
+          <label>닉네임</label>
+          <input type="text" {...register("name")} />
+          {errors.name && <span>{errors.name.message}</span>}
+        </div>
+        <div>
+          <label>이메일</label>
+          <input type="email" {...register("email")} />
+          {errors.email && <span>{errors.email.message}</span>}
+        </div>
+        <div>
+          <label>비밀번호</label>
+          <input type="password" {...register("password")} />
+          {errors.password && <span>{errors.password.message}</span>}
+        </div>
+        <div>
+          <label>비밀번호 확인</label>
+          <input type="password" {...register("confirmPassword")} />
+          {errors.confirmPassword && <span>{errors.confirmPassword.message}</span>}
+        </div>
+        <div>
+          <button type="submit">회원가입</button>
+        </div>
+      </form>
+    </section>
   );
 }
