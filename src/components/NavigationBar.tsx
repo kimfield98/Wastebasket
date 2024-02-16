dfdac995283023062227b70a622a214445bd69f0
kimfield98 <kimfield98@gmail.com>
2024-02-16 16:00:33 +0900

    [FIX]: (메인페이지 > 네비게이션 바 > 버튼) 로그인 시 임시 프로필 이미지로 변경

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -3,23 +3,32 @@ import Link from 'next/link'
 import { Button } from "./ui/button";
 import { useUserStore } from '@/stores/userStore';
 import { useEffect, useState } from 'react';
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 export default function NavigationBar() {
-  const [isButtonText, setButtonText] = useState("로그인")
+  const [isButton, setButton] = useState(true)
 
   useEffect(() => {
     if(useUserStore.getState().accessToken !== null) {
-      setButtonText("로그아웃")
+      setButton(false)
     }
   },[])
   
   return (
     <div className="flex justify-end items-center w-full h-14 py-3 px-4 bg-white">
-      <Link href={isButtonText === "로그인" ? "/login" : "/logout"}>
-        <Button className="h-8 w-[67px] text-sm bg-[#393F7B]">
-          {isButtonText}
-        </Button>
+      {isButton && 
+      <Button className="h-8 w-[67px] text-sm bg-[#393F7B]">
+        로그인
+      </Button>
+      }
+      {!isButton &&
+      <Link href="/mypage">
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
       </Link>
+      }
     </div>
   );
 }
