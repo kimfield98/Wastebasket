ed465e79e87d2495e53ed948800d3d5de4623967
kimfield98 <kimfield98@gmail.com>
2024-02-16 15:12:48 +0900

    [FEAT]: 로그아웃 구현 #8

---
+++ src/app/logout/page.tsx
@@ -0,0 +1,9 @@
+import LogoutComponent from "@/components/Logout";
+
+export default function Logout() {
+  return (
+    <div className="font-Pretendard">
+      <LogoutComponent />
+    </div>
+  );
+}
---
+++ src/components/Logout.tsx
@@ -0,0 +1,67 @@
+"use client"
+import axios from "axios";
+import Link from "next/link";
+import Image from 'next/image';
+import { useUserStore } from "@/stores/userStore";
+
+const LOGOUT_API = "http://13.125.226.155:3000/auth/logout";
+
+export default function LogoutComponent() {
+
+  // `handleSubmit` 사용 제거
+
+  async function handleLogout() {
+    const { accessToken, refreshToken } = useUserStore.getState();
+    try {
+      await axios.post(LOGOUT_API, {
+        accessToken,
+        refreshToken
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+      console.log('로그아웃 성공');
+      useUserStore.getState().setAccessToken("");
+      // 성공적으로 로그아웃 처리 후 페이지 이동
+      window.location.replace("/");
+    } catch (error) {
+      console.error(error);
+      alert("로그아웃에 실패했습니다. 다시 시도하세요."); // 에러 메시지 개선
+    }
+  }
+
+  return (
+      <section className="flex flex-col justify-center items-center min-h-screen w-[375px] bg-white">
+        <div className=''>
+          <div className='flex justify-between w-[328px] px-[24px] py-[15.5px]'>
+            <div>로그아웃</div>
+            <Link href="/">
+              <Image
+                src={"/assets/x.png"}
+                width={24}
+                height={24}
+                alt={"X"} 
+              />
+            </Link>
+          </div>
+          <div className='flex justify-center items-center w-[328px] h-[128px] border-y'>로그아웃 하시겠습니까?</div>
+          <div className='flex'>
+            <Link href="/mypage">
+              <button
+                className="w-[140px] h-[52px] mr-[16px] mt-4 border border-[#CACFD7] rounded"
+              >
+                취소
+              </button>
+            </Link>
+            <button
+              onClick={handleLogout} 
+              className="w-[140px] h-[52px] mt-4 bg-[#393F7B] rounded text-white font-semibold"
+            >
+              확인
+            </button>
+          </div>
+        </div>
+      </section>
+  );
+}     
