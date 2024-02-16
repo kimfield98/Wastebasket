e868edd46a3dd8ceb83008e377a0849d7ab5ea59
kimfield98 <kimfield98@gmail.com>
2024-02-16 14:26:31 +0900

    [ADD] (메인페이지 > 네비게이션 바 > 버튼) 토큰 유무에 따른 텍스트 변경

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,11 +1,24 @@
+"use client"
 import Link from 'next/link'
 import { Button } from "./ui/button";
+import { useUserStore } from '@/stores/userStore';
+import { useEffect, useState } from 'react';
 
 export default function NavigationBar() {
+  const [isButtonText, setButtonText] = useState("로그인")
+
+  useEffect(() => {
+    if(useUserStore.getState().accessToken !== null) {
+      setButtonText("로그아웃")
+    }
+  },[])
+  
   return (
-    <div className="flex justify-end items-center w-[100%] h-14 py-3 px-4 bg-white">
-      <Link href="/login">
-        <Button className="h-8 w-[67px] text-sm bg-[#393F7B]">로그인</Button>
+    <div className="flex justify-end items-center w-full h-14 py-3 px-4 bg-white">
+      <Link href={isButtonText === "로그인" ? "/login" : "/logout"}>
+        <Button className="h-8 w-[67px] text-sm bg-[#393F7B]">
+          {isButtonText}
+        </Button>
       </Link>
     </div>
   );
