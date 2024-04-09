e7cb98112424a7ffbefa361628261018c4382ae7
kimfield98 <kimfield98@gmail.com>
2024-04-09 21:43:30 +0900

    add: 스플래시 화면 생성

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+import Image from 'next/image'
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <main></main>
+    <main className='flex flex-col items-center justify-center h-screen pb-24'>
+      <Image
+        src="/assets/밥버디.png"
+        width={300}
+        height={500}
+        alt="스플래시 이미지"
+      />
+      <Link href="/login">
+        <button className='px-7 py-3 border border-[#758A94] rounded-full point-color font-bold cursor-pointer'>로그인 하기</button>
+      </Link>
+    </main>
   );
 }
