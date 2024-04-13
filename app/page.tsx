b525e99b3b5602038b0f56fd020f2172b4933efb
kimfield98 <kimfield98@gmail.com>
2024-04-13 18:40:55 +0900

    fix: Image 태그 속성 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,27 @@
-import Image from 'next/image'
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className='flex flex-col items-center justify-center h-screen pb-24'>
-      <Image
-        src="/assets/밥버디.png"
-        width={300}
-        height={500}
-        alt="스플래시 이미지"
-      />
-      <Link href="/login">
-        <button className='px-7 py-3 border border-[#758A94] rounded-full point-color font-bold cursor-pointer'>로그인 하기</button>
+      <div style={{ position: 'relative', width: '300px', height: '500px' }}>
+        <Image
+          alt='스플래시 이미지'
+          src='/assets/밥버디.png'
+          blurDataURL="/assets/밥버디.png"
+          placeholder='blur'
+          quality={100}
+          fill
+          sizes='100vw'
+          style={{
+            objectFit: "cover",
+          }}
+        />
+      </div>
+      <Link href='/login'>
+        <button className='px-7 py-3 border border-[#758A94] rounded-full point-color font-bold cursor-pointer'>
+          로그인 하기
+        </button>
       </Link>
     </main>
   );
