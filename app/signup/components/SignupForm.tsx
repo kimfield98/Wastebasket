774eebda4d1dd6e1902e3be59cb478b5db5a6164
kimfield98 <kimfield98@gmail.com>
2024-04-14 19:04:39 +0900

    add: 회원가입 페이지 퍼블리싱

---
+++ app/signup/components/SignupForm.tsx
@@ -0,0 +1,25 @@
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import Link from "next/link";
+
+const SignupPage = () => {
+  
+  return (
+    <form className="flex flex-col items-center">
+      <Input
+        placeholder="이름을 입력해주세요"
+      />
+      <Input
+        type="email"
+        placeholder="이메일을 입력해주세요"
+      />
+      <Input
+        type="password"
+        placeholder="비밀번호를 입력해주세요"
+      />
+      <Button className="w-80 bg-[#5F7A85]">회원가입</Button>
+      <Link href="/login" className="text-[#758A94] font-semibold">이미 계정이 있으신가요?</Link>
+    </form>
+  );
+};
+export default SignupPage;
