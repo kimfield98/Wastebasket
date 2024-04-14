d4610dbc6293dc26b5c0a937ce6882b229e38f61
kimfield98 <kimfield98@gmail.com>
2024-04-15 00:26:16 +0900

    add: 로그인 페이지 뒤로가기 버튼 추가

--- app/login/components/LoginForm.tsx
+++ app/login/components/LoginForm.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 
 
-const AdminLoginPage = () => {
+const LoginPage = () => {
   
   return (
     <form className="flex flex-col items-center">
@@ -16,9 +16,9 @@ const AdminLoginPage = () => {
         type="password"
         placeholder="비밀번호를 입력해주세요"
       />
-      <Button className="w-80 bg-[#5F7A85]">로그인</Button>
+      <Button className="w-80 my-5 bg-[#5F7A85]">로그인</Button>
       <Link href="/signup" className="text-[#758A94] font-semibold">아직 계정이 없으신가요? <span className="font-semibold underline">회원가입</span></Link>
     </form>
   );
 };
-export default AdminLoginPage;
+export default LoginPage;
