04930d5b065de285b44282855473d0aeff6d8b90
kimfield98 <kimfield98@gmail.com>
2024-04-17 15:23:05 +0900

    [Style] pretendard 폰트 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
 
-const inter = Inter({ subsets: ["latin"] });
+const Pretendard = localFont({
+  src: "./fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "100 900",
+});
 
 export const metadata: Metadata = {
   title: "kimfield world",
@@ -16,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={Pretendard.className}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main></main>
+    <main className='flex flex-col items-center justify-between h-screen pt-56 pb-32'>
+      <div className="flex flex-col items-center gap-1">
+        <div className="text-7xl">🌳</div>
+        <div className="text-3xl font-semibold">KIMFIELD WORLD</div>
+        <div className="text-xl">환영합니다!</div>
+      </div>
+      <div className="flex flex-col">
+        <button>시작하기</button>
+        <Link href='/login'>
+          <span>이미 계정이 있으신가요?</span>
+          <span>로그인</span>
+        </Link>
+      </div>
+    </main>
   );
 }
