607c0ee127bab606190ddb8200843d576883c457
kimfield <cw.kim@sparta-devcamp.com>
2024-04-17 13:48:43 +0900

    add: zod schema와 safeParse를 사용해 에러메시지 띄우기

---
+++ app/create-account/actions.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import { z } from "zod";
+
+const formSchema = z.object({
+  username: z.string().min(3).max(10),
+  email: z.string().email(),
+  password: z.string().min(10),
+  confirm_password: z.string().min(10),
+});
+
+export async function createAccount(prevState: any, formData: FormData) {
+  const data = {
+    userName: formData.get("userName"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+    passwordConfirm: formData.get("passwordConfirm"),
+  };
+  const result = formSchema.safeParse(data);
+  if (!result.success) {
+    return result.error.flatten();
+  }
+}
