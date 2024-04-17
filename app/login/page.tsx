47df88ee5ffb12cf21b23ff3f9595749902a6665
kimfield <cw.kim@sparta-devcamp.com>
2024-04-17 11:04:34 +0900

    add: server action 함수 예시

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,21 @@
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
+import { redirect } from "next/navigation";
 
 export default function LogIn() {
+  async function onSubmit(formData: FormData) {
+    "use server";
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    redirect("/profile")
+  }
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">이메일과 비밀번호를 입력해주세요</h2>
       </div>
-      <form className="flex flex-col gap-3">
+      <form action={onSubmit} className="flex flex-col gap-3">
         <FormInput name="email" type="email" placeholder="이메일" required errors={[]} />
         <FormInput
           name="password"
