a9d76ff7eac1dea5e5f0ec88399a0d84ad0d6fc2
kimfield98 <kimfield98@gmail.com>
2024-04-18 18:16:42 +0900

    [INIT] Next.js 프로젝트 생성

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "밥버디",
+  description: "점심시간 밥친구 랜덤매칭 플랫폼 밥버디",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
