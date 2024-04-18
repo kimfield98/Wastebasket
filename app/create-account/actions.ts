911203677912fb9f45ce7ce5e5fd90ebdcb279b8
kimfield98 <kimfield98@gmail.com>
2024-04-18 10:20:05 +0900

    [Add] zod validation 추가

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -4,6 +4,9 @@ import { z } from "zod";
 
 const checkUserName = (userName: string) => !userName.includes(" ");
 const checkPasswords = ({password, passwordConfirm}:{password: string, passwordConfirm: string}) => password === passwordConfirm;
+const passwordRegex = new RegExp(
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*?[#?!@$%^&*-]).+$/
+);
 
 const formSchema = z.object({
   username: z
@@ -13,13 +16,21 @@ const formSchema = z.object({
     })
     .min(3, { message: "이름은 2자 이상이어야 합니다." })
     .max(10, { message: "이름은 10자 이하여야 합니다." })
-    .refine(checkUserName, "이름에 공백이 포함되어 있습니다."),
+    // .refine(checkUserName, "이름에 공백이 포함되어 있습니다.")
+    // .transform((username) => `🔥 ${username}`)
+    .trim()
+    .toLowerCase(),
   email: z
     .string({ required_error: "이메일을 입력해주세요." })
-    .email({ message: "올바른 이메일 형식이 아닙니다." }),
+    .email({ message: "올바른 이메일 형식이 아닙니다." })
+    .toLowerCase(),
   password: z
     .string({ required_error: "비밀번호를 입력해주세요." })
-    .min(6, { message: "비밀번호는 6자 이상이어야 합니다." }),
+    .min(6, { message: "비밀번호는 6자 이상이어야 합니다." })
+    .regex(
+      passwordRegex,
+      "비밀번호는 대문자, 소문자, 숫자, 특수문자를 포함해야 합니다."
+    ),
     passwordConfirm: z
     .string()
     .min(6, { message: "비밀번호는 6자 이상이어야 합니다." }),
@@ -38,5 +49,7 @@ export async function createAccount(prevState: any, formData: FormData) {
   const result = formSchema.safeParse(data);
   if (!result.success) {
     return result.error.flatten();
+  } else {
+    console.log("회원가입 성공", result.data);
   }
 }
