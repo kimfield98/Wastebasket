6dbef51380fae1bb68ff45787f9f2e120e5dd15c
kimfield98 <kimfield98@gmail.com>
2024-04-19 00:24:52 +0900

    [DELETE] MUI 라이브러리 관련 코드 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Button from '@mui/material/Button';
-import Link from '@mui/material/Link';
 import Image from 'next/image';
 
 export default function Home() {
@@ -13,31 +11,6 @@ export default function Home() {
           height={2245}
           layout='responsive'
         />
-      </div>
-      <div className='flex flex-col gap-10 items-center justify-center'>
-        <Link href='/create-account'>
-          <Button
-            sx={{
-              color: 'white',
-              bgcolor: '#758A94',
-              ':focus': {
-                backgroundColor: '#758A94',
-              },
-              ':hover': {
-                backgroundColor: '#758A94',
-              },
-            }}
-            variant='contained'
-            size='large'
-          >
-            시작하기
-          </Button>
-        </Link>
-
-        <Link href='/login' color='inherit' underline='none'>
-          <span>이미 계정이 있나요?</span>
-          <span>&nbsp;로그인하기</span>
-        </Link>
       </div>
     </main>
   );
