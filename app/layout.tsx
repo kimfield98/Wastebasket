8c93bb276651857cd2223469b367be4aadbd9b3f
kimfield98 <kimfield98@gmail.com>
2024-04-18 19:22:31 +0900

    [Add] 메인페이지 퍼블리싱

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
-import type { Metadata } from "next";
-import localFont from 'next/font/local'
-import "./globals.css";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
 
 const Pretendard = localFont({
   src: './fonts/PretendardVariable.woff2',
   display: 'swap',
   weight: '100 900',
-})
+});
 
 export const metadata: Metadata = {
-  title: "밥버디",
-  description: "점심시간 밥친구 랜덤매칭 플랫폼 밥버디",
+  title: '밥버디',
+  description: '점심시간 밥친구 랜덤매칭 플랫폼 밥버디',
 };
 
 export default function RootLayout({
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={Pretendard.className}>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,44 @@
-import Image from "next/image";
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main></main>
+    <main className='flex flex-col items-center justify-center h-screen bg-[#e6ecf4] text-gray-600 font-medium'>
+      <div className='sm:w-[400px]'>
+        <Image
+          src='/밥버디.png'
+          alt='스플래시 이미지'
+          width={1587}
+          height={2245}
+          layout='responsive'
+        />
+      </div>
+      <div className='flex flex-col gap-10 items-center justify-center'>
+        <Link href='/create-account'>
+          <Button
+            sx={{
+              color: 'white',
+              bgcolor: '#758A94',
+              ':focus': {
+                backgroundColor: '#758A94',
+              },
+              ':hover': {
+                backgroundColor: '#758A94',
+              },
+            }}
+            variant='contained'
+            size='large'
+          >
+            시작하기
+          </Button>
+        </Link>
+
+        <Link href='/login' color='inherit' underline='none'>
+          <span>이미 계정이 있나요?</span>
+          <span>&nbsp;로그인하기</span>
+        </Link>
+      </div>
+    </main>
   );
 }
