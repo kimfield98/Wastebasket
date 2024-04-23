cc681487396b28b3ba087e797aba62bcbbaf0d94
kimfield98 <kimfield98@gmail.com>
2024-04-23 17:15:20 +0900

    useFormState 초기값과 action을 활용한 폼, 버튼 처리

--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -1,29 +1,45 @@
+'use client';
 import Button from '@/components/button';
 import Input from '@/components/input';
+import { useFormState } from 'react-dom';
+import { smsLogin } from './actions';
+
+const initialState = {
+  token: false,
+  error: undefined,
+};
 
 export default function SMSLogin() {
+  const [state, dispatch] = useFormState(smsLogin, initialState);
   return (
     <div className='flex flex-col gap-10 py-8 px-6'>
       <div className='flex flex-col gap-2 *:font-medium'>
         <h1 className='text-2xl'>SMS 로그인</h1>
-        <h2 className='text-xl'>전화번호를 입력해주세요.</h2>
+        <h2 className='text-xl'>전화번호와 인증번호를 입력해주세요.</h2>
       </div>
-      <form className='flex flex-col gap-3'>
-        <Input
-          name='phone'
-          type='number'
-          placeholder='전화번호'
-          required
-          errors={[]}
-        />
-        <Input
-          name='code'
-          type='number'
-          placeholder='인증번호'
-          required
-          errors={[]}
-        />
-        <Button text='인증하기' />
+      <form action={dispatch} className='flex flex-col gap-3'>
+        {state.token ? (
+          <Input
+            key='token'
+            name='token'
+            type='number'
+            placeholder='인증번호'
+            required
+            min={100000}
+            max={999999}
+            errors={state.error?.formErrors}
+          />
+        ) : (
+          <Input
+            key='phone'
+            name='phone'
+            type='text'
+            placeholder='전화번호'
+            required
+            errors={state.error?.formErrors}
+          />
+        )}
+        <Button text={state.token ? '인증하기' : '인증번호 요청'} />
       </form>
     </div>
   );
