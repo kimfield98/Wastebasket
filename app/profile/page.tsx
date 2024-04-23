fda3d1144bffc19ffe9e93aa4e472c097eb52fb8
kimfield98 <kimfield98@gmail.com>
2024-04-23 18:39:17 +0900

    프로필 페이지 유저 정보 불러오기

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,27 @@
-export default function Profile() {
-  return <h1>프로필 페이지입니다.</h1>;
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { notFound } from "next/navigation";
+
+async function getUser() {
+  const session = await getSession();
+  if (session.id) {
+    const user = await db.user.findUnique({
+      where: {
+        id: session.id,
+      },
+    });
+    if (user) {
+      return user;
+    }
+  }
+  notFound();
+}
+
+export default async function Profile() {
+  const user = await getUser();
+  return (
+    <div>
+      <h1>{user?.username}의 프로필</h1>
+    </div>
+  );
 }
