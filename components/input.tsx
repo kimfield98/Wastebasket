0c9a00d0ffbda6675ff03842ffe1d3064133e6fd
kimfield98 <kimfield98@gmail.com>
2024-04-23 16:59:53 +0900

    InputHTMLAttributes<HTMLInputElement> 타입 추가 후 기본 타입 제거

--- components/input.tsx
+++ components/input.tsx
@@ -1,26 +1,21 @@
+import { InputHTMLAttributes } from 'react';
+
 interface InputProps {
   name: string;
-  type: string;
-  placeholder: string;
-  required: boolean;
   errors?: string[];
 }
 
 export default function Input({
   name,
-  type,
-  placeholder,
-  required,
   errors = [],
-}: InputProps) {
+  ...rest
+}: InputProps & InputHTMLAttributes<HTMLInputElement>) {
   return (
     <div className='flex flex-col gap-2'>
       <input
         name={name}
         className='bg-transparent rounded-md w-full h-10 px-3 focus:outline-none ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-yellow-500 border-none placeholder:text-neutral-400'
-        type={type}
-        placeholder={placeholder}
-        required={required}
+        {...rest}
       />
       {errors.map((error, index) => (
         <span key={index} className='text-red-500 font-medium'>
