0be05ae7a9906b7f4d31ee40fe196245c292a4e4
kimfield98 <kimfield98@gmail.com>
2024-04-25 11:49:19 +0900

    변수명 변경 및 쓰지 않는 코드 삭제

--- app/(tabs)/profile/actions.ts
+++ app/(tabs)/profile/actions.ts
@@ -4,8 +4,7 @@ import getSession from '@/lib/session';
 import { redirect } from 'next/navigation';
 
 export const logout = async () => {
-  'use server';
-  const session = await getSession();
-  await session.destroy();
+  const cookie = await getSession();
+  await cookie.destroy();
   redirect('/');
 };
