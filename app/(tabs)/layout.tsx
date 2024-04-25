ed5c5f568c467b5964b71dd42a2ab2eb09cc7a70
kimfield98 <kimfield98@gmail.com>
2024-04-25 19:17:36 +0900

    하단 네비게이션 바 배경색 추가 + 자식 컴포넌트 하단 패딩 추가

--- app/(tabs)/layout.tsx
+++ app/(tabs)/layout.tsx
@@ -3,7 +3,7 @@ import TabBar from '@/components/tab-bar';
 export default function TabLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <div className='px-5 py-10'>{children}</div>
+      <div className='px-5 pt-10 pb-24'>{children}</div>
       <TabBar />
     </div>
   );
