7dbce7aee880aea7b0d580a4bd4b8a205e8e40fc
kimfield98 <kimfield98@gmail.com>
2024-04-28 14:58:22 +0900

    3) 이미지 업로드 - label 태그 사용해서 이미지 미리보기 구현

--- app/match/add/page.tsx
+++ app/match/add/page.tsx
@@ -3,23 +3,32 @@
 import Button from '@/components/button';
 import Input from '@/components/input';
 import { PhotoIcon } from '@heroicons/react/24/solid';
+import { useState } from 'react';
 
 export default function AddProduct() {
+  const [previewImage, setPreviewImage] = useState('');
   const uploadImage = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) return;
     const file = e.target.files[0];
     const url = URL.createObjectURL(file);
-    console.log(url);
+    setPreviewImage(url);
   };
   return (
     <div>
       <form className='p-5 flex flex-col gap-5'>
         <label
           htmlFor='image'
-          className='border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer'
+          className='border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer bg-center bg-cover bg-no-repeat'
+          style={{ backgroundImage: `url(${previewImage})` }}
         >
-          <PhotoIcon className='w-20' />
-          <div className='text-neutral-400 text-sm'>사진을 추가해주세요.</div>
+          {previewImage === '' ? (
+            <>
+              <PhotoIcon className='w-20' />
+              <div className='text-neutral-400 text-sm'>
+                사진을 추가해주세요.
+              </div>
+            </>
+          ) : null}
         </label>
         <input
           onChange={uploadImage}
