2012a1c584a2bbd643b1a045251b56c77212780d
kimfield98 <kimfield98@gmail.com>
2024-05-24 00:52:33 +0900

    사용자 현재 위치 불러오기

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # 개발 일지
 - 5/18(토) : 프로젝트 기획, repository 생성, vercel 배포
 - 5/21(화) : NAVER CLOUD PLATFORM `Web Dynamic Map` ClientID 받아서 지도 띄우기 🗺️
+- 5/23(목) : 사용자 현재 위치 불러오기
--- app/page.tsx
+++ app/page.tsx
@@ -1,71 +1,71 @@
-// 클라이언트 측에서만 렌더링 되기를 바랄 때 붙이는 'use client'
 'use client'
 import Script from "next/script";
 import { useEffect, useRef, useState } from "react";
+import { useGeoLocation } from "./hooks/useGeoLocation";
 
-// window 객체에 naver 속성을 추가하기 위한 타입 정의
 declare global {
   interface Window {
     naver: any;
   }
 }
 
+// 객체는 사용자의 위치를 가져오는 데 사용되는 옵션 설정
+const geolocationOptions = {
+  enableHighAccuracy: true, // 위치를 가능한 높은 정확도로 가져올지 여부
+  timeout: 1000 * 10, // 위치를 가져오기 위한 최대 시간(밀리초)을 설정
+  maximumAge: 1000 * 3600 * 24, // 이전에 가져온 위치 정보가 유효한 시간(밀리초)을 설정
+}
+
 export default function Home() {
-  // 지도 DOM 요소를 참조하기 위한 ref
   const mapElement = useRef(null);
-
-  // 네이버 맵의 초기 위치
-  const latitude = 37.5665;  // 예시: 서울시청 위도
-  const longitude = 126.9780; // 예시: 서울시청 경도
-
-  // 네이버 맵스 스크립트가 로드되었는지 여부를 추적하기 위한 상태
+  const { location, error } = useGeoLocation(geolocationOptions)
+  const [currentLocation, setCurrentLocation] = useState({latitude: 37.5665, longitude: 126.9780}) // 예시: 서울시청 위도, 경도
   const [naverMapsLoaded, setNaverMapsLoaded] = useState(false);
   
-  // 네이버 맵스 스크립트 로드 여부를 체크하는 useEffect
   useEffect(() => {
     const intervalId = setInterval(() => {
-      // window 객체에 naver와 naver.maps가 존재하는지 확인
       if (window.naver && window.naver.maps) {
-        // 스크립트가 로드되었다면 상태를 업데이트하고 인터벌 정리
         setNaverMapsLoaded(true);
-        clearInterval(intervalId); // 인터벌을 정리하여 체크 중지
+        clearInterval(intervalId);
       }
-    }, 100); // 100ms마다 체크
+    }, 100);
 
-    // 컴포넌트가 unmount되었을 때 인터벌 정리
     return () => clearInterval(intervalId);
-  }, []); // 의존성 배열이 빈 배열이므로 컴포넌트가 처음 렌더링될 때만 실행
+  }, []);
 
-  // naverMapsLoaded가 true가 되면 지도를 그리기 위한 useEffect
+  // naverMapsLoaded가 true이고,
+  // 사용자 위치가 변경되었을 때 지도를 업데이트하는 useEffect
   useEffect(() => {
-    if (mapElement.current && naverMapsLoaded) {
-      const { naver } = window; // window 객체에서 naver를 가져옴
-      const location = new naver.maps.LatLng(latitude, longitude); // 네이버 지도에서 사용하는 LatLng 객체 생성
+    if (location && naverMapsLoaded && mapElement.current) {
+      const { naver } = window;
+      const locationCoords = new naver.maps.LatLng(location.latitude, location.longitude); // 네이버 지도에서 사용하는 LatLng 객체 생성
       const mapOptions = {
-        center: location, // 지도의 중심을 설정
-        zoom: 16, // 줌 레벨 설정
-        zoomControl: false, // 줌 컨트롤 비활성화
+        center: locationCoords,
+        zoom: 16,
+        zoomControl: false,
       };
-      const map = new naver.maps.Map(mapElement.current, mapOptions); // 지도 생성
+      const map = new naver.maps.Map(mapElement.current, mapOptions);
       new naver.maps.Marker({
-        position: location, // 마커 위치 설정
+        position: locationCoords, // 마커 위치 설정
         map: map, // 마커를 추가할 지도
       });
+      setCurrentLocation(location); // 현재 위치 상태를 업데이트
     }
-  }, [naverMapsLoaded]); // naverMapsLoaded 값이 변경될 때마다 이 효과가 다시 실행
+  }, [location, naverMapsLoaded]); // location과 naverMapsLoaded 값이 변경될 때마다 이 효과가 다시 실행
 
   return (
     <>
       <Script
         src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID}`}
-        strategy="beforeInteractive" // 스크립트를 페이지가 인터랙티브 되기 전에 로드
+        strategy="beforeInteractive"
         onLoad={() => {
           if (window.naver && window.naver.maps) {
-            setNaverMapsLoaded(true); // 스크립트가 로드된 후 상태 업데이트
+            setNaverMapsLoaded(true);
           }
         }}
       />
       <div id="mapContainer" ref={mapElement}></div>
+      {error && <p>Error: {error}</p>}
     </>
   );
 }
