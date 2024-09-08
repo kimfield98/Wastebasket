a5747a9686047685aa8390e788ea725901769c26
kimfield98 <kimfield98@gmail.com>
2024-09-08 12:19:14 +0900

    ➕ 크로마틱 설치
    배포 및 워크플로우 파일 추가

--- src/components/ui/alert-dialog/alert-dialog.tsx
+++ src/components/ui/alert-dialog/alert-dialog.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
 
 import { cn } from "@/lib/utils"
-import { buttonVariants } from "../button/button"
+import { buttonVariants } from "../button/button-variants"
 
 const AlertDialog = AlertDialogPrimitive.Root
 
