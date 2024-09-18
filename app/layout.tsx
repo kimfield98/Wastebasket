d25feeafd940e73497348dc648d52302cd3e9cc6
kimfield98 <kimfield98@gmail.com>
2024-09-18 20:44:28 +0900

    💄 블로그 레이아웃 변경
    상단 Navbar를 Sidebar로 변경하였고, 간단히 프로필을 기재할 공간을 추가하였습니다.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,11 @@ import './global.css'
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
-import { Navbar } from './components/nav'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
-import Footer from './components/footer'
 import { baseUrl } from './sitemap'
 import { themeEffect } from 'utils/theme-effect'
+import { Sidebar } from './components/side-bar'
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
@@ -60,11 +59,10 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-          <Navbar />
+      <body className="antialiased">
+        <Sidebar />
+        <main className="ml-64 p-8">
           {children}
-          <Footer />
           <Analytics />
           <SpeedInsights />
         </main>
